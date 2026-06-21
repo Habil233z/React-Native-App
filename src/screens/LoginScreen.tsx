1816c775d1328e10b0530495e10f03806b5269ed
Habil <habilherdianto233z@gmail.com>
2026-06-21 18:34:37 +0700

    finish styling LoginScreen

--- src/screens/LoginScreen.tsx
+++ src/screens/LoginScreen.tsx
@@ -1,5 +1,5 @@
 import { useContext, useState } from "react";
-import { View, Text, Button, TextInput, Alert } from "react-native";
+import { View, Text, Button, TextInput, Alert, TouchableOpacity } from "react-native";
 import { api } from "../config/api";
 import { AuthContext } from "../../App";
 
@@ -24,20 +24,14 @@ export default function LoginScreen({navigation}: any) {
 
     return (
         <View className="flex-1 bg-gray-100 justify-center items-center">
-            <View className="flex-1 bg-white justify-center items-center m-h-150 px-5 rounded-xl">
-                <Text>Login to Socidata Analytics App</Text>
-                <View>
-                <TextInput
-                placeholder="Email or Username"
-                onChangeText={setEmailOrUsername}
-                ></TextInput>
-                <TextInput
-                placeholder="Password"
-                secureTextEntry={true}
-                onChangeText={setPassword}
-                ></TextInput>
-                </View>
-                <Button title="Login" onPress={handleClick}/>
+            <View className="bg-white justify-center items-center m-h-150 px-5 rounded-xl border border-gray-400 pb-4">
+                <Text className="font-bold text-blue-700 text-4xl mt-4">SociNet</Text>
+                <Text className="mt-2 font-semibold">Login to SociNet Data Analytics App</Text>
+                    <View className="w-[70%] items-start mt-2 mb-4">
+                        <View className="flex-row border border-gray-400 bg-gray-200 h-8 mb-2"><TextInput className="p-0 w-full h-full pl-2" placeholder="Email or Username" onChangeText={setEmailOrUsername}/></View>
+                        <View className="flex-row border border-gray-400 bg-gray-200 h-8"><TextInput className="p-0 w-full h-full pl-2" placeholder="Password" secureTextEntry={true} onChangeText={setPassword}/></View>
+                    </View>
+                <TouchableOpacity className="bg-gray-200 border border-l-gray-700 h-8 w-[50px] items-center justify-center" activeOpacity={0.5} onPress={handleClick}><Text>Login</Text></TouchableOpacity>
             </View>
         </View>
     )
